5167803547c061d40900601aa5eb29e1dbb70a89
Raftouch <rafanelkin@gmail.com>
2023-05-23 18:04:38 +0200

    :sparkles: added routes, views

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Author = require('../models/author')
+const Author = require("../models/author");
 
 // All authors route
 router.get("/", (req, res) => {
@@ -8,13 +8,26 @@ router.get("/", (req, res) => {
 });
 
 // New author route
-router.get('/new', (req, res) => {
-    res.render("authors/new", { author: new Author() });
-})
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
 
 // Create author route
-router.post('/', (req, res) => {
-    res.send('Create')
-})
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+
+  try {
+    const newAuthor = await author.save();
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect("authors");
+  } catch (error) {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author",
+    });
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require('body-parser')
+const port = process.env.PORT
+const url = process.env.DATABASE_URL
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
@@ -14,9 +17,12 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL);
+mongoose.connect(url, {
+  useNewUrlParser: true
+});
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to mongoose"));
@@ -24,4 +30,4 @@ db.once("open", () => console.log("Connected to mongoose"));
 app.use("/", indexRouter);
 app.use("/authors", authorRouter)
 
-app.listen(process.env.PORT || 3000);
+app.listen(port, () => console.log(`App is listening on port ${port}`));
