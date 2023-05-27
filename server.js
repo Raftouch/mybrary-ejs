406c52fca1345037c768fcd3f7f84b7cd52595ae
Raftouch <rafanelkin@gmail.com>
2023-05-27 17:21:44 +0200

    :bricks: finished author routes

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
 const port = process.env.PORT
 const url = process.env.DATABASE_URL
 
@@ -16,9 +18,10 @@ const bookRouter = require('./routes/books')
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
+
 app.use(expressLayouts);
+app.use(methodOverride('_method'))
 app.use(express.static("public"));
-// app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 app.use(express.urlencoded({ limit: '10mb', extended: false}))
 
 const mongoose = require("mongoose");
